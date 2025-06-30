812173a3e24dfeaf6a2806f009db1cfbbb360782
emmassjk <emmanuel24062@gmail.com>
2025-06-29 22:51:24 -0400

    agregados los metodos hateoas

--- src/main/java/com/inventario/controllers/InventarioControllers.java
+++ src/main/java/com/inventario/controllers/InventarioControllers.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import com.inventario.dto.InventarioDTO;
 import com.inventario.servicies.InventarioServicies;
 
@@ -44,5 +47,40 @@ public class InventarioControllers
     public ResponseEntity<Void> eliminar(@PathVariable Integer id) {
         inventarioService.eliminar(id);
         return ResponseEntity.noContent().build();
+    }
+
+    // MÉTODO HATEOAS para buscar por ID
+    @GetMapping("/hateoas/{id}")
+    public InventarioDTO obtenerHATEOAS(@PathVariable Integer id) {
+        InventarioDTO dto = inventarioService.obtenerPorId(id);
+
+        // Links de la misma API
+        dto.add(linkTo(methodOn(InventarioControllers.class).obtenerHATEOAS(id)).withSelfRel());
+        dto.add(linkTo(methodOn(InventarioControllers.class).obtenerTodosHATEOAS()).withRel("todos"));
+        dto.add(linkTo(methodOn(InventarioControllers.class).eliminar(id)).withRel("eliminar"));
+
+        // Links HATEOAS para API Gateway "A mano"
+        dto.add(org.springframework.hateoas.Link.of("http://localhost:8888/api/proxy/inventario/" + dto.getId()).withSelfRel());
+        dto.add(org.springframework.hateoas.Link.of("http://localhost:8888/api/proxy/inventario/" + dto.getId()).withRel("Modificar HATEOAS").withType("PUT"));
+        dto.add(org.springframework.hateoas.Link.of("http://localhost:8888/api/proxy/inventario/" + dto.getId()).withRel("Eliminar HATEOAS").withType("DELETE"));
+
+        return dto;
+    }
+
+    // MÉTODO HATEOAS para listar todos los inventarios utilizando HATEOAS
+    @GetMapping("/hateoas")
+    public List<InventarioDTO> obtenerTodosHATEOAS() {
+        List<InventarioDTO> lista = inventarioService.listar();
+
+        for (InventarioDTO dto : lista) {
+            // Link de la misma API
+            dto.add(linkTo(methodOn(InventarioControllers.class).obtenerHATEOAS(dto.getId())).withSelfRel());
+
+            // Links HATEOAS para API Gateway "A mano"
+            dto.add(org.springframework.hateoas.Link.of("http://localhost:8888/api/proxy/inventario").withRel("Get todos HATEOAS"));
+            dto.add(org.springframework.hateoas.Link.of("http://localhost:8888/api/proxy/inventario/" + dto.getId()).withRel("Crear HATEOAS").withType("POST"));
+        }
+
+        return lista;
     }
 }
--- src/main/java/com/inventario/dto/InventarioDTO.java
+++ src/main/java/com/inventario/dto/InventarioDTO.java
@@ -1,12 +1,13 @@
 package com.inventario.dto;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class InventarioDTO 
-{
+public class InventarioDTO extends RepresentationModel<InventarioDTO> {
     private Integer id;
     private Integer idProducto;
     private Integer stockDisponible;
